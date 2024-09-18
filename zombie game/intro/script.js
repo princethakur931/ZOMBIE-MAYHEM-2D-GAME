c062cf3e81972aec5de27d61e7fd85a147ef9cfe
PRINCE THAKUR <142495134+princethakur931@users.noreply.github.com>
2024-09-18 21:38:41 +0530

    Add files via upload

---
+++ zombie game/intro/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("startButton").addEventListener("click", function() {
+    // Redirect to the game URL
+    window.location.href = "http://127.0.0.1:5500/zombie-mayhemhtml5-js-and-scss-game/dist/index.html";
+});
